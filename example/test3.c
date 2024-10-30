f190cad8d964d9d574bacfdf7ef650a6b055b427
Florian Lüke <f.lueke@mesytec.com>
2024-10-30 12:22:59 +0100

    test3: reporting now prints transaction counts, retries, etc

--- example/test3.c
+++ example/test3.c
@@ -1,63 +1,119 @@
 #include <mvlcc_readout_config.h>
 #include <mvlcc_wrap.h>
 #include <stdio.h>
+#include <signal.h>
+
+// On SIGUP force printing a report, on SIGINT or SIGTERM quit.
+volatile int g_do_quit ;
+volatile int g_do_report;
+
+void signal_handler(int signum)
+{
+    if (signum == SIGHUP)
+        g_do_report = 1;
+    else
+        g_do_quit = 1;
+}
+
+int setup_signal_handlers()
+{
+    /* Set up the structure to specify the new action. */
+    struct sigaction new_action;
+    new_action.sa_handler = signal_handler;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = 0;
+
+    const int sigInterest[] = { SIGINT, SIGHUP, SIGTERM };
+
+    for (int i = 0; i < sizeof(sigInterest) / sizeof(sigInterest[0]); i++)
+    {
+        if (sigaction(sigInterest[i], &new_action, NULL) != 0)
+            return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[]){
-  mvlcc_t mvlc = NULL;
-  int ec;
-  int i;
-  int tell = 1;
+    mvlcc_t mvlc = NULL;
+    int ec;
+    uint64_t i;
+    int tell = 1;
 
-  if (argc <= 1)
+    if (argc <= 1)
     {
-      fprintf(stderr, "Device not specified.\n");
-      return 1;
+        fprintf(stderr, "Device not specified.\n");
+        return 1;
     }
 
-  mvlc = mvlcc_make_mvlc(argv[1]);
-
-  if (ec = mvlcc_connect(mvlc))
+    if (setup_signal_handlers())
     {
-      fprintf(stderr, "Could not connect.\n");
-      return 1;
+        fprintf(stderr, "Could not set up signal handlers.\n");
+        return 1;
     }
 
-  uint16_t regAddr = 0x1304u; // the controller/crate id register
-  uint32_t readValue = 0u;
-  const uint32_t writeValue = 0b11;
+    mvlc = mvlcc_make_mvlc(argv[1]);
 
-  for (i = 0; i < 100000; i++)
-  {
-    ec = mvlcc_register_read(mvlc, regAddr, &readValue);
-
-    if (ec)
+    if (ec = mvlcc_connect(mvlc))
     {
-      fprintf(stderr, "Could not read internal register @ 0x%04x: %s.\n", regAddr, mvlcc_strerror(ec));
-      return 1;
+        fprintf(stderr, "Could not connect.\n");
+        return 1;
     }
 
-    ec = mvlcc_register_write(mvlc, regAddr, writeValue);
+    uint16_t regAddr = 0x1304u; // the controller/crate id register
+    uint32_t readValue = 0u;
+    const uint32_t writeValue = 0b11;
+    int ret = 0;
 
-    if (ec)
+    for (i = 0; true; i++)
     {
-      fprintf(stderr, "Could not write internal register @ 0x%04x: %s.\n", regAddr, mvlcc_strerror(ec));
-      return 1;
+        ec = mvlcc_register_write(mvlc, regAddr, writeValue);
+
+        if (ec)
+        {
+            fprintf(stderr, "Could not write internal register @ 0x%04x: %s.\n", regAddr, mvlcc_strerror(ec));
+            ret = 1;
+            break;
+        }
+
+        ec = mvlcc_register_read(mvlc, regAddr, &readValue);
+
+        if (ec)
+        {
+            fprintf(stderr, "Could not read internal register @ 0x%04x: %s.\n", regAddr, mvlcc_strerror(ec));
+            ret = 1;
+            break;
+        }
+
+        if (i == tell || g_do_report)
+        {
+            fprintf(stdout, "cycle=%lu, ", i);
+            mvlcc_print_mvlc_cmd_counters(stdout, mvlc);
+            fprintf(stdout, "\n");
+
+            fflush(stdout);
+            if (!g_do_report)
+            {
+                if (tell < 5000)
+                    tell = tell * 2;
+                else
+                    tell += 5000;
+            }
+
+            g_do_report = 0;
+        }
+
+        if (g_do_quit)
+            break;
     }
 
-    if (i == tell)
-    {
-      printf("%d  ", i);
-      fflush(stdout);
-      if (tell < 5000)
-        tell = tell * 2;
-      else
-        tell += 5000;
-    }
-  }
+    printf("Final stats:\n");
+    fprintf(stdout, "cycle=%lu, ", i);
+    mvlcc_print_mvlc_cmd_counters(stdout, mvlc);
+    fprintf(stdout, "\n");
 
-  printf("\n");
-  mvlcc_disconnect(mvlc);
-  mvlcc_free_mvlc(mvlc);
+    mvlcc_disconnect(mvlc);
+    mvlcc_free_mvlc(mvlc);
 
-  return 0;
+    return ret;
 }
